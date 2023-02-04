3a759fdbfee0b3e245ca9431e0fcfaed6f3da0d7
mrkenkre <mrkenkre@gmail.com>
2023-02-04 11:20:55 -0500

    Timer changes.

--- src/main/java/edu/neu/coe/info6205/util/Timer.java
+++ src/main/java/edu/neu/coe/info6205/util/Timer.java
@@ -57,7 +57,17 @@ public class Timer {
     public <T, U> double repeat(int n, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
         logger.trace("repeat: with " + n + " runs");
         // FIXME: note that the timer is running when this method is called and should still be running when it returns. by replacing the following code
-         return 0;
+        for(int i=0;i<n;i++) {
+        	 T t = supplier.get();
+             if (preFunction != null) {
+                 t = preFunction.apply(t);
+             }		
+             U res = function.apply(t);
+             if (postFunction != null) {
+                 postFunction.accept(res);
+             }
+        }
+         return meanLapTime();
         // END 
     }
 
@@ -177,7 +187,7 @@ public class Timer {
      */
     private static long getClock() {
         // FIXME by replacing the following code
-         return 0;
+         return System.nanoTime();
         // END 
     }
 
@@ -190,7 +200,7 @@ public class Timer {
      */
     private static double toMillisecs(long ticks) {
         // FIXME by replacing the following code
-         return 0;
+         return System.currentTimeMillis();
         // END 
     }
 
